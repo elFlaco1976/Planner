51ee806d81185ff40ec7936e7a548d7f80b92b03
Smouska <sandragjacinto@gmail.com>
2017-07-06 11:40:38 -0400

    Select and Checkbox
    Script change for select listener and get id from selected recipes

--- Francisco/code/ingredients api tests.js
+++ Francisco/code/ingredients api tests.js
@@ -27,17 +27,33 @@ getRecipes(userKeyword);
 });
 
 //listener for accept selection field
-console.log("before accept listener");
+var SelectedRecipe = [];
+var countChecked = function() {
+  var n = $( "input:checked" ).length;
+  $( "div" ).text( n + (n === 1 ? " is" : " are") + " checked!" );
+};
+countChecked();
+
+$( "input[type=checkbox]" ).on( "click", countChecked );
+
 $('#AcceptSelectionBtn').click(function(){
-         console.log ('olafsdfdsf');
-
-    /*$('#currentRecipes').children('li').each(function(i) { 
-     
-     console.log ('ola');
-
-     //$(this).rotate(ring.stones[i].stone_rotation);
-    }); */
-});
+      SelectedRecipe = [];
+         console.log ('Listener to select OK');
+         $( "input:checked" ).each(function(i){
+           SelectedRecipe.push(($(this).attr('id')));
+         });
+         ExtractRecipeId (SelectedRecipe);
+         console.log(SelectedRecipe);
+       })
+    //clicked element, do-some-stuff
+var RecipeIds = [];
+function ExtractRecipeId (SelectedRecipe){
+  for (i=0; i<(SelectedRecipe.length); ++i){
+    var RecipeId = SelectedRecipe[i].split("_")[1];
+    RecipeIds.push(RecipeId);
+    console.log(RecipeIds);
+  }
+}
 
 
 function appendParameterToUrl(url, parameterName, parameterValue)
@@ -63,37 +79,31 @@ function onSearchResponse(data)
     console.log("Answer from fetch:" + currentRecipes);
 
     //Kind of user selection. Just select first n recipes for now
-    selectRecipes();
+    populateRecipeList();
 
     var rawIngredients = getIngredientsFromRecipes(currentRecipesSelected);
 
-    htmlFillRawIngredientsList(rawIngredients);
+    //todo change place for this: htmlFillRawIngredientsList(rawIngredients);
 }
-//todo: rename this. rather is displaying all recipes
-function selectRecipes()
+
+function populateRecipeList()
 {
-   var userRecipeSelection = 5;//prompt("Select recipes. First x recipes:");
-    var incRecipeSelection = 0;
     for(var i in currentRecipes)
     {
         var contentRecipeTitle = currentRecipes[i].recipe.label;
         let li = document.createElement("li");
 /*        let liContent = document.createTextNode(contentRecipeTitle);
-        li.appendChild(liContent) 
+        li.appendChild(liContent)
         htmlRecipeList.appendChild(li); */
-        incRecipeSelection++;
-        var CheckboxIdIndex = "ChooseRecipe_"+i
-        $('#currentRecipes').append('<li class="list-group-item">'+'<h5>'+contentRecipeTitle+'</h5>'+ '<input type="checkbox" ' + "id="+ '\''+ CheckboxIdIndex+ '\'' +'>'); 
 
-        var testId = '<li class="list-group-item" id=RecipeItem>'+'<h5>'+contentRecipeTitle+'</h5>'+ '<input type="checkbox" ' + "id="+ '\''+ CheckboxIdIndex+ '\'' +'>'; 
+        var CheckboxIdIndex = "ChooseRecipe_"+i
+        $('#currentRecipes').append('<li class="list-group-item">'+'<h5>'+contentRecipeTitle+'</h5>'+ '<input type="checkbox" ' + "id="+ '\''+ CheckboxIdIndex+ '\'' +'>');
+
+        var testId = '<li class="list-group-item" id=RecipeItem>'+'<h5>'+contentRecipeTitle+'</h5>'+ '<input type="checkbox" ' + "id="+ '\''+ CheckboxIdIndex+ '\'' +'>';
        // console.log (testId)
 
         currentRecipesSelected.push(currentRecipes[i]);
-        if(incRecipeSelection >= userRecipeSelection)
-        {
-            break;
-        }
-    }
+    }        
 
 
 
@@ -104,7 +114,7 @@ function getIngredientsFromRecipes(recipes)
 {
     var ingredients = [];
     for(var i in recipes)
-    {    
+    {
         //Concatenates ingredient list of one recipe to total list of ingredients
         ingredients.push.apply(ingredients, getIngredientsFromRecipe(recipes[i]));
     }
@@ -117,7 +127,7 @@ function getIngredientsFromRecipe(recipe)
     var ingredients = [];
     for(var i in ingredientLines)
     {
-        //Builds ingredient list 
+        //Builds ingredient list
         ingredients.push(ingredientLines[i]);
     }
     return ingredients;
