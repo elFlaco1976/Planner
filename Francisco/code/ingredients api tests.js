73f19f6ced5c56418acf77a0ddc535ee6746cd63
Smouska <sandragjacinto@gmail.com>
2017-07-07 15:55:35 -0400

    tabbs

--- Francisco/code/ingredients api tests.js
+++ Francisco/code/ingredients api tests.js
@@ -38,9 +38,9 @@ $(document).ready(function () {
         //   console.log('SelectedRecipe' + SelectedRecipe); //debug
         //   console.log('RecipeIds' + RecipeIds); //debug
         populateIngredFromId(RecipeIds); // populate list
-        populateTitles( temporarySelectedRecipes )
+        populateTitles(temporarySelectedRecipes)
         //UpdateSelectedRecipes(TemporarySelectedRecipes);
-       
+
     })
 
     function ExtractRecipeId(selectedRecipe, RecipeIds) {
@@ -66,121 +66,121 @@ $(document).ready(function () {
         }
     }
 
-        var temporarySelectedRecipes = [];
-        function populateIngredFromId(RecipeIds) {
-            temporarySelectedRecipes = [];
-            //console.log('PopulateIngredFromId RecipeIds ' + RecipeIds); //debug
-            for (i = 0; i < (RecipeIds.length); i++) {
-                temporarySelectedRecipes.push(currentRecipes[RecipeIds[i]]);
+    var temporarySelectedRecipes = [];
+    function populateIngredFromId(recipeIds) {
+        temporarySelectedRecipes = [];
+        //console.log('PopulateIngredFromId RecipeIds ' + RecipeIds); //debug
+        for (i = 0; i < recipeIds.length; i++) {
+            temporarySelectedRecipes.push(currentRecipes[recipeIds[i]]);
+        }
+        //console.log('PopulateIngredFromId TemporarySelectedRecipes' + TemporarySelectedRecipes); //debug
+        var rawIngredients = getIngredientsFromRecipes(temporarySelectedRecipes);
+        htmlFillRawIngredientsList(rawIngredients);
+
+    }
+
+    function populateTitles(recipesTT) {
+        var rawTitlesSelectedRecipe = getTitlesFromRecipes(recipesTT);
+        //console.log(rawTitlesSelectedRecipe); //debug
+        htmlFillRawTitlesList(rawTitlesSelectedRecipe);
+    }
+
+    function appendParameterToUrl(url, parameterName, parameterValue) {
+        return url + "&" + parameterName + "=" + parameterValue;
+    }
+
+    function getRecipes(keyword) {
+        var urlSearch = baseUrlSearch + keyword;
+        fetch(urlSearch)
+            .then(function (response) { return response.json(); })
+            .then(function (data) {
+                onSearchResponse(data);
             }
-            //console.log('PopulateIngredFromId TemporarySelectedRecipes' + TemporarySelectedRecipes); //debug
-            var rawIngredients = getIngredientsFromRecipes(temporarySelectedRecipes);
-            htmlFillRawIngredientsList(rawIngredients);
+            );
+    }
 
+    function onSearchResponse(data) {
+        currentRecipes = data.hits;
+        //console.log("Answer from fetch:" + currentRecipes); //debug
+        //Kind of user selection. Just select first n recipes for now
+        populateRecipeList();
+    }
+
+    function populateRecipeList() {
+        for (var i in currentRecipes) {
+            var contentRecipeTitle = currentRecipes[i].recipe.label;
+            let li = document.createElement("li");
+            var checkboxIdIndex = "ChooseRecipe_" + i
+            $('#currentRecipes').append('<li class="list-group-item" id="listeRecettes">' + '<h5>' + contentRecipeTitle + '</h5>' + '<input class=checkRec type="checkbox" ' + "id=" + '\'' + checkboxIdIndex + '\'' + '>');
+            currentRecipesDisplayed.push(currentRecipes[i]);
+        }
+    }
+
+    function getIngredientsFromRecipes(recipes) {
+        var ingredients = [];
+        for (var i in recipes) {
+            //Concatenates ingredient list of one recipe to total list of ingredients
+            ingredients.push.apply(ingredients, getIngredientsFromRecipe(recipes[i]));
         }
 
-        function populateTitles(recipesTT) {
-            var rawTitlesSelectedRecipe = getTitlesFromRecipes(recipesTT);
-            //console.log(rawTitlesSelectedRecipe); //debug
-            htmlFillRawTitlesList(rawTitlesSelectedRecipe);
+        return ingredients;
+    }
+
+    function getIngredientsFromRecipe(recipe) {
+        var ingredientLines = recipe.recipe.ingredientLines;
+        //console.log(recipe); //debug
+        var ingredients = [];
+        for (var i in ingredientLines) {
+            //Builds ingredient list
+            ingredients.push(ingredientLines[i]);
         }
 
-        function appendParameterToUrl(url, parameterName, parameterValue) {
-            return url + "&" + parameterName + "=" + parameterValue;
+        return ingredients;
+    }
+
+
+    function getTitlesFromRecipes(recipes) {
+        var Titles = [];
+        for (var i in recipes) {
+            //Concatenates ingredient list of one recipe to total list of Titles
+            Titles.push(getTitleFromRecipe(recipes[i]));
         }
+        // console.log(Titles);//debug
+        return Titles;
+    }
 
-        function getRecipes(keyword) {
-            var urlSearch = baseUrlSearch + keyword;
-            fetch(urlSearch)
-                .then(function (response) { return response.json(); })
-                .then(function (data) {
-                    onSearchResponse(data);
-                }
-                );
+    function getTitleFromRecipe(recipe) {
+        var Recipetitle = recipe.recipe.label;
+        //console.log(Recipetitle); //debug
+        return Recipetitle;
+    }
+
+
+
+    //Fills html ul list
+    function htmlFillRawIngredientsList(rawIngredients) {
+        $('#currentIngredients').empty(); //empties the list to refill it again
+        for (var i in rawIngredients) {
+            var li = document.createElement("li");
+            var liContent = document.createTextNode(rawIngredients[i]);
+            li.appendChild(liContent);
+            htmlIngredientList.appendChild(li);
         }
+    }
 
-        function onSearchResponse(data) {
-            currentRecipes = data.hits;
-            //console.log("Answer from fetch:" + currentRecipes); //debug
-            //Kind of user selection. Just select first n recipes for now
-            populateRecipeList();
+    //Fills html ul list
+    function htmlFillRawTitlesList(rawTitlesSelectedRecipe) {
+        console.log('htmlFillRawTitlesList ' + rawTitlesSelectedRecipe);
+        $('#SelecteRecipeList').empty(); //empties the list to refill it again
+        for (var i in rawTitlesSelectedRecipe) {
+            var li = document.createElement("li");
+            li.className = "list-group-item";
+            li.setAttribute("id", "RecipeItem");
+            var liContent = document.createTextNode(rawTitlesSelectedRecipe[i]);
+            li.appendChild(liContent);
+            htmlRecipeTitlesList.appendChild(li);
         }
-
-        function populateRecipeList() {
-            for (var i in currentRecipes) {
-                var contentRecipeTitle = currentRecipes[i].recipe.label;
-                let li = document.createElement("li");
-                var checkboxIdIndex = "ChooseRecipe_" + i
-                $('#currentRecipes').append('<li class="list-group-item" id="listeRecettes">' + '<h5>' + contentRecipeTitle + '</h5>' + '<input class=checkRec type="checkbox" ' + "id=" + '\'' + checkboxIdIndex + '\'' + '>');
-                currentRecipesDisplayed.push(currentRecipes[i]);
-            }
-        }
-
-        function getIngredientsFromRecipes(recipes) {
-            var ingredients = [];
-            for (var i in recipes) {
-                //Concatenates ingredient list of one recipe to total list of ingredients
-                ingredients.push.apply(ingredients, getIngredientsFromRecipe(recipes[i]));
-            }
-            
-            return ingredients;
-        }
-
-        function getIngredientsFromRecipe(recipe) {
-            var ingredientLines = recipe.recipe.ingredientLines;
-            //console.log(recipe); //debug
-            var ingredients = [];
-            for (var i in ingredientLines) {
-                //Builds ingredient list
-                ingredients.push(ingredientLines[i]);
-            }
-            
-            return ingredients;
-        }
+    }
 
 
-        function getTitlesFromRecipes(recipes) {
-            var Titles = [];
-            for (var i in recipes) {
-                //Concatenates ingredient list of one recipe to total list of Titles
-                Titles.push(getTitleFromRecipe(recipes[i]));
-            }
-           // console.log(Titles);//debug
-            return Titles;
-        }
-
-        function getTitleFromRecipe(recipe) {
-            var Recipetitle = recipe.recipe.label;
-            //console.log(Recipetitle); //debug
-            return Recipetitle;
-        }
-
-
-
-        //Fills html ul list
-        function htmlFillRawIngredientsList(rawIngredients) {
-            $('#currentIngredients').empty(); //empties the list to refill it again
-            for (var i in rawIngredients) {
-                var li = document.createElement("li");
-                var liContent = document.createTextNode(rawIngredients[i]);
-                li.appendChild(liContent);
-                htmlIngredientList.appendChild(li);
-            }
-        }
-
-        //Fills html ul list
-        function htmlFillRawTitlesList(rawTitlesSelectedRecipe) {
-            console.log( 'htmlFillRawTitlesList ' +rawTitlesSelectedRecipe);
-            $('#SelecteRecipeList').empty(); //empties the list to refill it again
-            for (var i in rawTitlesSelectedRecipe) {
-                var li = document.createElement("li");
-                li.className = "list-group-item";
-                li.setAttribute("id", "RecipeItem");
-                var liContent = document.createTextNode(rawTitlesSelectedRecipe[i]);
-                li.appendChild(liContent);
-                htmlRecipeTitlesList.appendChild(li);
-            }
-        }
-
-
-    })
+})
